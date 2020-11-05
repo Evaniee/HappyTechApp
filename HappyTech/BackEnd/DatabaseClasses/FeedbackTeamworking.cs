cb0a396d28b29f40e3f6a242636c26e7a7429ce3
Harry Rippon <410403@edu.cwa.ac.uk>
2020-11-05 09:24:51 +0000

    Added Teamworking database classes

---
+++ HappyTech/BackEnd/DatabaseClasses/FeedbackTeamworking.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HappyTech.BackEnd.DatabaseClasses
+{
+    class FeedbackTeamworking
+    {
+        private int m_feedback_teamworking_id;
+
+        public int feedback_questions_id
+        {
+            get
+            {
+                return m_feedback_teamworking_id;
+            }
+        }
+
+        private int m_feedback_id;
+
+        public int feedback_id
+        {
+            get
+            {
+                return m_feedback_id;
+            }
+
+            set
+            {
+                m_feedback_id = value;
+            }
+        }
+
+        private int m_teamworking_id;
+
+        public int teamworking_id
+        {
+            get
+            {
+                return m_feedback_id;
+            }
+
+            set
+            {
+                m_teamworking_id = value;
+            }
+        }
+
+        private bool m_achived;
+
+        public bool archived
+        {
+            get
+            {
+                return m_achived;
+            }
+
+            set
+            {
+                m_achived = value;
+            }
+        }
+    }
+}
---
+++ HappyTech/BackEnd/DatabaseClasses/Teamworking.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HappyTech.BackEnd.DatabaseClasses
+{
+    class Teamworking
+    {
+        private int m_teamworking_id;     
+
+        public int teamworking_id
+        {
+            get
+            {
+                return m_teamworking_id;
+            }
+
+            set
+            {
+                m_teamworking_id = value;
+            }
+        }
+
+        private string m_teamworking;    
+
+        public string teamworking
+        {
+            get
+            {
+                return m_teamworking;
+            }
+            set
+            {
+                m_teamworking = value;
+            }
+        }
+    }
+}
