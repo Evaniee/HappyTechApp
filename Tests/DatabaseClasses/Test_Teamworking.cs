f37fabed622afdfeb11a6f2bc18d7faaeabfe6c8
Harry Rippon <410403@edu.cwa.ac.uk>
2020-11-09 14:54:12 +0000

    Teamworking Test

--- Tests/DatabaseClasses/Test_Teamworking.cs
+++ Tests/DatabaseClasses/Test_Teamworking.cs
@@ -1,13 +1,36 @@
-﻿using System;
+﻿using Accessibility;
+using HappyTech.BackEnd.DatabaseClasses;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
-using NUnit.Framework;
 
 namespace Tests.DatabaseClasses
 {
     [TestFixture]
     class Test_Teamworking
     {
+        private Teamworking m_teamworking;
+
+        [SetUp]
+        public void Setup()
+        {
+            m_teamworking = new Teamworking();
+        }
+
+        [Test]
+        public void Test_teamworking_id()
+        {
+            m_teamworking.teamworking_id = 1;
+            Assert.AreEqual(m_teamworking.teamworking_id, 1);
+        }
+
+        [Test]
+        public void Test_teamworking()
+        {
+            m_teamworking.teamworking = "Test teamworking";
+            Assert.AreEqual(m_teamworking.teamworking, "Test teamworking");
+        }
 
     }
 }
