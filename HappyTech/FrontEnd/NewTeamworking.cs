a7cc7532566ed83ecce4eec55dac4e3db3f2e0b7
Harry Rippon <410403@edu.cwa.ac.uk>
2020-11-09 16:12:48 +0000

    Lastest update

---
+++ HappyTech/FrontEnd/NewTeamworking.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HappyTech.FrontEnd
+{
+    public partial class NewTeamworking : Form
+    {
+        public NewTeamworking()
+        {
+            InitializeComponent();
+        }
+
+        private void NewTeamworking_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
