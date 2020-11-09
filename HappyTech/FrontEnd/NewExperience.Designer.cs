ed7ecfa9f76f08745de760a5526853fd8178594a
Adam Hockney <409380@edu.cwa.ac.uk>
2020-11-09 12:26:53 +0000

    NewExperiences created

--- HappyTech/FrontEnd/NewExperience.Designer.cs
+++ HappyTech/FrontEnd/NewExperience.Designer.cs
@@ -28,33 +28,178 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.cmb_test = new System.Windows.Forms.ComboBox();
+            this.txt_experience = new System.Windows.Forms.TextBox();
+            this.lbl_experience = new System.Windows.Forms.Label();
+            this.txt_reponse1 = new System.Windows.Forms.TextBox();
+            this.lbl_response1 = new System.Windows.Forms.Label();
+            this.txt_reponse2 = new System.Windows.Forms.TextBox();
+            this.lbl_response2 = new System.Windows.Forms.Label();
+            this.txt_reponse3 = new System.Windows.Forms.TextBox();
+            this.lbl_response3 = new System.Windows.Forms.Label();
+            this.lbl_response4 = new System.Windows.Forms.Label();
+            this.txt_reponse4 = new System.Windows.Forms.TextBox();
+            this.lbl_response5 = new System.Windows.Forms.Label();
+            this.txt_reponse5 = new System.Windows.Forms.TextBox();
+            this.btn_submit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // cmb_test
+            // txt_experience
             // 
-            this.cmb_test.AllowDrop = true;
-            this.cmb_test.FormattingEnabled = true;
-            this.cmb_test.Location = new System.Drawing.Point(100, 107);
-            this.cmb_test.Name = "cmb_test";
-            this.cmb_test.Size = new System.Drawing.Size(121, 23);
-            this.cmb_test.TabIndex = 0;
-            this.cmb_test.DropDown += new System.EventHandler(this.cmb_test_DropDown);
+            this.txt_experience.Location = new System.Drawing.Point(12, 113);
+            this.txt_experience.Multiline = true;
+            this.txt_experience.Name = "txt_experience";
+            this.txt_experience.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txt_experience.Size = new System.Drawing.Size(776, 68);
+            this.txt_experience.TabIndex = 0;
+            // 
+            // lbl_experience
+            // 
+            this.lbl_experience.AutoSize = true;
+            this.lbl_experience.Location = new System.Drawing.Point(369, 95);
+            this.lbl_experience.Name = "lbl_experience";
+            this.lbl_experience.Size = new System.Drawing.Size(67, 15);
+            this.lbl_experience.TabIndex = 2;
+            this.lbl_experience.Text = "Experience:";
+            // 
+            // txt_reponse1
+            // 
+            this.txt_reponse1.Location = new System.Drawing.Point(84, 187);
+            this.txt_reponse1.Multiline = true;
+            this.txt_reponse1.Name = "txt_reponse1";
+            this.txt_reponse1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txt_reponse1.Size = new System.Drawing.Size(704, 23);
+            this.txt_reponse1.TabIndex = 1;
+            // 
+            // lbl_response1
+            // 
+            this.lbl_response1.AutoSize = true;
+            this.lbl_response1.Location = new System.Drawing.Point(12, 190);
+            this.lbl_response1.Name = "lbl_response1";
+            this.lbl_response1.Size = new System.Drawing.Size(69, 15);
+            this.lbl_response1.TabIndex = 2;
+            this.lbl_response1.Text = "Response1: ";
+            // 
+            // txt_reponse2
+            // 
+            this.txt_reponse2.Location = new System.Drawing.Point(84, 216);
+            this.txt_reponse2.Multiline = true;
+            this.txt_reponse2.Name = "txt_reponse2";
+            this.txt_reponse2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txt_reponse2.Size = new System.Drawing.Size(704, 23);
+            this.txt_reponse2.TabIndex = 2;
+            // 
+            // lbl_response2
+            // 
+            this.lbl_response2.AutoSize = true;
+            this.lbl_response2.Location = new System.Drawing.Point(12, 219);
+            this.lbl_response2.Name = "lbl_response2";
+            this.lbl_response2.Size = new System.Drawing.Size(69, 15);
+            this.lbl_response2.TabIndex = 2;
+            this.lbl_response2.Text = "Response 2:";
+            // 
+            // txt_reponse3
+            // 
+            this.txt_reponse3.Location = new System.Drawing.Point(84, 245);
+            this.txt_reponse3.Multiline = true;
+            this.txt_reponse3.Name = "txt_reponse3";
+            this.txt_reponse3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txt_reponse3.Size = new System.Drawing.Size(704, 23);
+            this.txt_reponse3.TabIndex = 3;
+            // 
+            // lbl_response3
+            // 
+            this.lbl_response3.AutoSize = true;
+            this.lbl_response3.Location = new System.Drawing.Point(12, 248);
+            this.lbl_response3.Name = "lbl_response3";
+            this.lbl_response3.Size = new System.Drawing.Size(69, 15);
+            this.lbl_response3.TabIndex = 2;
+            this.lbl_response3.Text = "Response 3:";
+            // 
+            // lbl_response4
+            // 
+            this.lbl_response4.AutoSize = true;
+            this.lbl_response4.Location = new System.Drawing.Point(12, 277);
+            this.lbl_response4.Name = "lbl_response4";
+            this.lbl_response4.Size = new System.Drawing.Size(64, 15);
+            this.lbl_response4.TabIndex = 2;
+            this.lbl_response4.Text = "Reponse 4:";
+            // 
+            // txt_reponse4
+            // 
+            this.txt_reponse4.Location = new System.Drawing.Point(84, 274);
+            this.txt_reponse4.Multiline = true;
+            this.txt_reponse4.Name = "txt_reponse4";
+            this.txt_reponse4.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txt_reponse4.Size = new System.Drawing.Size(704, 23);
+            this.txt_reponse4.TabIndex = 4;
+            // 
+            // lbl_response5
+            // 
+            this.lbl_response5.AutoSize = true;
+            this.lbl_response5.Location = new System.Drawing.Point(12, 306);
+            this.lbl_response5.Name = "lbl_response5";
+            this.lbl_response5.Size = new System.Drawing.Size(66, 15);
+            this.lbl_response5.TabIndex = 2;
+            this.lbl_response5.Text = "Response 5";
+            // 
+            // txt_reponse5
+            // 
+            this.txt_reponse5.Location = new System.Drawing.Point(84, 303);
+            this.txt_reponse5.Multiline = true;
+            this.txt_reponse5.Name = "txt_reponse5";
+            this.txt_reponse5.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txt_reponse5.Size = new System.Drawing.Size(704, 23);
+            this.txt_reponse5.TabIndex = 5;
+            // 
+            // btn_submit
+            // 
+            this.btn_submit.Location = new System.Drawing.Point(369, 332);
+            this.btn_submit.Name = "btn_submit";
+            this.btn_submit.Size = new System.Drawing.Size(75, 23);
+            this.btn_submit.TabIndex = 6;
+            this.btn_submit.Text = "Submit";
+            this.btn_submit.UseVisualStyleBackColor = true;
+            this.btn_submit.Click += new System.EventHandler(this.btn_submit_Click);
             // 
             // NewExperience
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.cmb_test);
+            this.Controls.Add(this.btn_submit);
+            this.Controls.Add(this.txt_reponse5);
+            this.Controls.Add(this.lbl_response5);
+            this.Controls.Add(this.txt_reponse4);
+            this.Controls.Add(this.lbl_response4);
+            this.Controls.Add(this.lbl_response3);
+            this.Controls.Add(this.txt_reponse3);
+            this.Controls.Add(this.lbl_response2);
+            this.Controls.Add(this.txt_reponse2);
+            this.Controls.Add(this.lbl_response1);
+            this.Controls.Add(this.txt_reponse1);
+            this.Controls.Add(this.lbl_experience);
+            this.Controls.Add(this.txt_experience);
             this.Name = "NewExperience";
             this.Text = "NewExperience";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ComboBox cmb_test;
+        private System.Windows.Forms.TextBox txt_experience;
+        private System.Windows.Forms.Label lbl_experience;
+        private System.Windows.Forms.TextBox txt_reponse1;
+        private System.Windows.Forms.Label lbl_response1;
+        private System.Windows.Forms.TextBox txt_reponse2;
+        private System.Windows.Forms.Label lbl_response2;
+        private System.Windows.Forms.TextBox txt_reponse3;
+        private System.Windows.Forms.Label lbl_response3;
+        private System.Windows.Forms.Label lbl_response4;
+        private System.Windows.Forms.TextBox txt_reponse4;
+        private System.Windows.Forms.Label lbl_response5;
+        private System.Windows.Forms.TextBox txt_reponse5;
+        private System.Windows.Forms.Button btn_submit;
     }
 }
--- HappyTech/FrontEnd/NewExperience.cs
+++ HappyTech/FrontEnd/NewExperience.cs
@@ -1,4 +1,5 @@
-﻿using HappyTech.BackEnd;
+﻿using HappyTech.BackEnd.DatabaseClasses;
+using HappyTech.BackEnd.FormBackEnds;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -6,38 +7,72 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
-using HappyTech.BackEnd.DatabaseClasses;
 
 namespace HappyTech.FrontEnd
 {
     public partial class NewExperience : Form
     {
-        public NewExperience()
+        NewExperienceBackEnd m_newExperienceBackEnd;
+
+        /// <summary>
+        /// Constructor for New Experience Front End
+        /// </summary>
+        /// <param name="a_templateExperienceBackEnd">Associated Back End</param>
+        /// <param name="a_experience">Experience to populate form with (Nullable)</param>
+        public NewExperience(TemplateExperienceBackEnd a_templateExperienceBackEnd, Experience a_experience)
         {
+            new NewExperienceBackEnd(this, a_templateExperienceBackEnd);
             InitializeComponent();
+            m_newExperienceBackEnd.Populate(a_experience);
         }
 
-        private void cmb_test_DropDown(object sender, EventArgs e)
+        /// <summary>
+        /// Connect to an associated NewExperienceBackEnd
+        /// </summary>
+        /// <param name="a_newExperienceBackEnd">Associated NewExperienceBackEnd</param>
+        public void Connect(NewExperienceBackEnd a_newExperienceBackEnd)
         {
-            List<string> l_comboValues = new List<string>();
-            l_comboValues.Add("New Experience");
-            foreach (Experience i_experience in BuisnessMetaLayer.Instance.GetDBExperience())
-            {
-                l_comboValues.Add(i_experience.experience_id + ": " + i_experience.experience);
-            }
-
-            // Initialize combo box
-            var comboBox = new ComboBox
-            {
-                DataSource = l_comboValues,
-                Location = new System.Drawing.Point(12, 12),
-                Name = "comboBox",
-                Size = new System.Drawing.Size(166, 21),
-                DropDownStyle = ComboBoxStyle.DropDownList
-            };
-
-            // Add the combo box to the form.
-            this.Controls.Add(comboBox);
+            m_newExperienceBackEnd = a_newExperienceBackEnd;
         }
+
+        /// <summary>
+        /// Populate text boxes with existing experience data
+        /// </summary>
+        /// <param name="a_experience">Experience of new experience</param>
+        /// <param name="a_response1">Response 1 of new experience</param>
+        /// <param name="a_response2">Response 2 of new experience</param>
+        /// <param name="a_response3">Response 3 of new experience</param>
+        /// <param name="a_response4">Response 4 of new experience</param>
+        /// <param name="a_response5">Response 5 of new experience</param>
+        public void Populate(string a_experience, string a_response1, string a_response2, string a_response3, string a_response4, string a_response5)
+        {
+            if (a_experience != null)
+                txt_experience.Text = a_experience;
+            if (a_response1 != null)
+                txt_reponse1.Text = a_response1;
+            if (a_response2 != null)
+                txt_reponse2.Text = a_response2;
+            if (a_response3 != null)
+                txt_reponse3.Text = a_response3;
+            if (a_response4 != null)
+                txt_reponse4.Text = a_response4;
+            if (a_response5 != null)
+                txt_reponse5.Text = a_response5;
+        }
+
+        /// <summary>
+        /// Occurs when the submit button is clicked
+        /// </summary>
+        /// <param name="sender">Object that sent event</param>
+        /// <param name="e">Event arguments</param>
+        public void btn_submit_Click(object sender, EventArgs e)
+        {
+            m_newExperienceBackEnd.Submit(txt_experience.Text, txt_reponse1.Text, txt_reponse2.Text, txt_reponse3.Text, txt_reponse4.Text, txt_reponse5.Text);
+        }
+
+        /// <summary>
+        /// Getter for back end
+        /// </summary>
+        public NewExperienceBackEnd BackEnd { get { return m_newExperienceBackEnd; } }
     }
 }
